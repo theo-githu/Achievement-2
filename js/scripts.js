c39b614e58652dfd00f25ba64d40c16ce3db2070
theo-githu <111968864+theo-githu@users.noreply.github.com>
2022-09-08 17:40:20 +0100

    Update scripts.js
    remove weight and add abilities

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ let pokemonList = [ // blank array of pokemon
 {
     name: 'Venusaur',
     height: 2,
-    weight: '100kg', 
+    abilities: ['chlorophyll', 'overgrow'], 
     types: ['grass', 'poison']
 },
 {
     name: 'Charizard',
     height: 1.7,
-    weight: '90.5kg',
+    abilities: ['blaze', 'solar-power'],
     types: ['fire', 'flying']
 },
 {
     name: 'Vileplume',
     height: 1.2,
-    weight: '18.6kg',
+    abilities: ['effect-spore', 'chlorophyll'],
     types: ['grass', 'poison']
 }   
 ];

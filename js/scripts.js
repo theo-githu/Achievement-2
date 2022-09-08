375d2dd4c5e95f86c8e08159dc15465a344ba2e3
theo-githu <111968864+theo-githu@users.noreply.github.com>
2022-09-08 17:33:28 +0100

    Update scripts.js
    add pokemon data in blank array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-alert('Hello World');
-
-let favouriteFood = 'braai';
-document.write(favouriteFood);
+let pokemonList = [ // blank array of pokemon
+{
+    name: 'Venusaur',
+    height: 2,
+    weight: '100kg', 
+    types: ['grass', 'poison']
+},
+{
+    name: 'Charizard',
+    height: 1.7,
+    weight: '90.5kg',
+    types: ['fire', 'flying']
+},
+{
+    name: 'Vileplume',
+    height: 1.2,
+    weight: '18.6kg',
+    types: ['grass', 'poison']
+}   
+];

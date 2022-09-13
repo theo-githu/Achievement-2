9a1cbb72119dd4689b0c101d8b659ee111f902df
theo-githu <111968864+theo-githu@users.noreply.github.com>
2022-09-13 11:57:43 +0100

    Update scripts.js
    add new condition to loop for smallest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
-let pokemonList = [ 
-{
-    name: 'Venusaur',
-    height: 2,
-    abilities: ['chlorophyll', 'overgrow'], 
-    types: ['grass', 'poison']
-},
-{
-    name: 'Charizard',
-    height: 1.7,
-    abilities: ['blaze', 'solar-power'],
-    types: ['fire', 'flying']
-},
-{
-    name: 'Vileplume',
-    height: 1.2,
-    abilities: ['effect-spore', 'chlorophyll'],
-    types: ['grass', 'poison']
-}   
+let pokemonList = [
+    {
+        name: 'Venusaur',
+        height: 2,
+        abilities: ['chlorophyll', 'overgrow'],
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        abilities: ['blaze', 'solar-power'],
+        types: ['fire', 'flying']
+    },
+    {
+        name: 'Vileplume',
+        height: 1.2,
+        abilities: ['effect-spore', 'chlorophyll'],
+        types: ['grass', 'poison']
+    }
 ];
 
-for (let i=0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + '  - Height: '+ pokemonList[i].height + 'm')
-  }
+let height = ' - Height: ';
+let m = 'm';
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 1.3) {
+        document.write(pokemonList[i].name + height + pokemonList[i].height + m + ' is the smallest pokemon.');
+    } else {
+        document.write(pokemonList[i].name + height + pokemonList[i].height + m + '<br>');
+    }
+}

2ea5210052b86ab100b5308105fcf44d63a3f6a4
theo-githu <111968864+theo-githu@users.noreply.github.com>
2022-09-16 12:13:23 +0100

    Update scripts.js
    Wrap pokemonList in IIFE. Create new variable, pokemonRepository.
    Return object with getAll and add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,44 @@
-let pokemonList = [
-    {
-        name: 'Venusaur',
-        height: 2,
-        abilities: ['chlorophyll', 'overgrow'],
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        abilities: ['blaze', 'solar-power'],
-        types: ['fire', 'flying']
-    },
-    {
-        name: 'Vileplume',
-        height: 1.2,
-        abilities: ['effect-spore', 'chlorophyll'],
-        types: ['grass', 'poison']
-    }
-];
+let pokemonRepository = (function () {
 
-// forEach() loops 
-let height = ' - Height: '
-pokemonList.forEach(function (pokemon) {
-    document.write(pokemon.name + height + pokemone.height + 'm' + '<br>');
-});
+
+    let pokemonList = [
+        {
+            name: 'Venusaur',
+            height: 2,
+            abilities: ['chlorophyll', 'overgrow'],
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            abilities: ['blaze', 'solar-power'],
+            types: ['fire', 'flying']
+        },
+        {
+            name: 'Vileplume',
+            height: 1.2,
+            abilities: ['effect-spore', 'chlorophyll'],
+            types: ['grass', 'poison']
+        }
+    ];
+
+    // forEach() loops 
+    let height = ' - Height: '
+    pokemonList.forEach(function (pokemon) {
+        document.write(pokemon.name + height + pokemon.height + 'm' + '<br>');
+    });
+
+    return {
+        add: function (pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function () {
+            return pokemonList;
+        }
+    };
+})();
+
+console.log(pokemonRepository.getAll()); //[]
+pokemonRepository.add({ name: 'Mew' });
+console.log(pokemonRepository.getAll()); //[{name: 'Mew'}]
+

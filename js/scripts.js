447cbd015147a87999ba0eb1ca5163759a8d4e64
theo-githu <111968864+theo-githu@users.noreply.github.com>
2022-09-18 17:00:25 +0100

    Update scripts.js
    replace array with API.
    load data from API and return to browser with specified data

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,6 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {
-            name: 'Venusaur',
-            height: 2,
-            weight: '100kg',
-            abilities: ['chlorophyll', 'overgrow'],
-            types: ['grass', 'poison']
-        },
-        {
-            name: 'Charizard',
-            weight: '90.5kg',
-            height: 1.7,
-            abilities: ['blaze', 'solar-power'],
-            types: ['fire', 'flying']
-        },
-        {
-            name: 'Vileplume',
-            weight: '18.6kg',
-            height: 1.2,
-            abilities: ['effect-spore', 'chlorophyll'],
-            types: ['grass', 'poison']
-        }
-    ];
-
-    // Function adds pokemon and validates typeof    
-    // function add(pokemon) {
-    //     if (typeof pokemon !== 'object') {
-    //         console.log('Please assign a Pokemon');
-    //     }
-    //     let keys = Object.keys(pokemon);
-    //     if (!keys.includes('name')) {
-    //         console.log('Requirements are missing');
-    //     }
-    //     pokemonList.push(pokemon);
-    // }
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
         if (
@@ -68,8 +35,42 @@ let pokemonRepository = (function () {
         });
     }
 
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+                //console.log(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            item.imageUrl = details.sprites.front_default;
+            item.weight = details.weight;
+            item.height = details.height;
+            item.abilities = details.abilities;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
     function showDetails(pokemon) {
-        console.log(pokemon);
+        pokemonRepository.loadDetails(item).then(function () {
+            console.log(pokemon);
+        });
     }
 
     // filter function. Starts at 0 as array
@@ -82,19 +83,34 @@ let pokemonRepository = (function () {
         getAll: getAll,
         add: add,
         addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
         showDetail: showDetails
     };
 })();
 
-console.log(pokemonRepository.getAll()); //[]
-pokemonRepository.add({ name: 'Mew', height: 0.4, weight: '4kg', abilities: ['Synchronize'], types: ['psychic'] });
-console.log(pokemonRepository.getAll()); //[{name: 'Mew'}]
+// code now obsolete as information is from API
+// console.log(pokemonRepository.getAll()); //[]
+// pokemonRepository.add({ name: 'Mew', height: 0.4, weight: '4kg', abilities: ['Synchronize'], types: ['psychic'] });
+// console.log(pokemonRepository.getAll()); //[{name: 'Mew'}]
 
 // forEach() loops 
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
 
-new Promise((resolve, reject) => {
-    
-})
+
+
+   // Function adds pokemon and validates typeof    
+    // function add(pokemon) {
+    //     if (typeof pokemon !== 'object') {
+    //         console.log('Please assign a Pokemon');
+    //     }
+    //     let keys = Object.keys(pokemon);
+    //     if (!keys.includes('name')) {
+    //         console.log('Requirements are missing');
+    //     }
+    //     pokemonList.push(pokemon);
+    // }
